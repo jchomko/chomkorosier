7a0572583cec033ae4c5cc8b5d13d8667358b834
aarongillett <mail@aarongillett.com>
2016-08-08 23:13:12 +0100

    Slideshow Settings

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,34 @@
 !function($) {
 
-      $(document).ready(function(){
-        $('.slick-slider').slick({
-          accessibility: true,
-          arrows: false,
-          dots: true,
-          fade: true,
-          customPaging : function(slider, i) {
-      var thumb = $(slider.$slides[i]).data();
-      return '<a>'+(i+1)+'</a>';
-              },
-          });
+  $(document).ready(function(){
+    $('.slick-slider').slick({
+      accessibility: true,
+      arrows: false,
+      dots: true,
+      fade: true,
+      customPaging : function(slider, i) {
+  var thumb = $(slider.$slides[i]).data();
+  return '<a>'+(i+1)+'</a>';
+          },
       });
+  });
 
-   //custom function showing current slide
-        var $status = $('.pagingInfo');
-        var $slickElement = $('.slider');
+  // custom function showing current slide
+    var $status = $('.pagingInfo');
+    var $slickElement = $('.slider');
 
-        $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-            //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-            var i = (currentSlide ? currentSlide : 0) + 1;
-            $status.text(i + '/' + slick.slideCount);
-        });
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + '/' + slick.slideCount);
+    });
+
+  // click to advance slide
+
+    $('.a-slide').click(function() {
+      $('.slick-slider').slick('slickGoTo', parseInt($('.slick-slider').slick('slickCurrentSlide'))+1);
+    });
+
+  // end
 
 }(window.jQuery);
