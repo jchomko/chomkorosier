e29be60d14175440d792d80142a17bde5c5ebc3c
aarongillett <mail@aarongillett.com>
2016-08-08 01:08:37 +0100

    content, header.php

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,26 @@
 !function($) {
-  // Write your own javascript here.
+
+      $(document).ready(function(){
+        $('.slick-slider').slick({
+          accessibility: true,
+          arrows: false,
+          dots: true,
+          fade: true,
+          customPaging : function(slider, i) {
+      var thumb = $(slider.$slides[i]).data();
+      return '<a>'+(i+1)+'</a>';
+              },
+          });
+      });
+
+   //custom function showing current slide
+        var $status = $('.pagingInfo');
+        var $slickElement = $('.slider');
+
+        $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+            //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            $status.text(i + '/' + slick.slideCount);
+        });
+
 }(window.jQuery);
